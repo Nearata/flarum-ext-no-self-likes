17a9d8e946e5345fd391a54d1f8245c3d9ccada7
William Di Cicco <nearata@protonmail.com>
2021-04-05 16:42:25 +0200

    Using the policy extender
    * Simplified the code a lot

--- extend.php
+++ extend.php
@@ -3,23 +3,9 @@
 namespace Nearata\NoSelfLikes;
 
 use Flarum\Extend;
-use Flarum\Foundation\ValidationException;
-
-use Flarum\Likes\Event\PostWasLiked;
+use Flarum\Post\Post;
 
 return [
-    (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js'),
-    new Extend\Locales(__DIR__ . '/resources/locale'),
-    (new Extend\Event())
-        ->listen(PostWasLiked::class, function (PostWasLiked $event) {
-            $post = $event->post;
-            $actor = $event->actor;
-            $actorId = $actor->id;
-
-            if ($post->user_id === $actorId) {
-                $post->likes()->detach($actorId);
-                throw new ValidationException(['noselflikes' => app('translator')->trans('nearata-no-self-likes.forum.error_message')]);
-            }
-        })
+    (new Extend\Policy())
+        ->modelPolicy(Post::class, PostPolicy::class)
 ];
